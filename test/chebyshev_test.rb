e2f3da9c695f34e71a60571b84c9e3b674dcc145
gogotanaka <mail@tanakakazuki.com>
2016-01-29 01:39:13 +0900

    Add test assert

--- test/chebyshev_test.rb
+++ test/chebyshev_test.rb
@@ -1,11 +1,7 @@
 require 'test_helper'
 
 class ChebyshevTest < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::Chebyshev::VERSION
-  end
-
-  def test_it_does_something_useful
-    assert false
+  def test_main
+    assert_equal "test testtest test\n", Chebyshev.expand("test/files/testfile", "test/files/test.yml")
   end
 end
