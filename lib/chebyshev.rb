2f0cbb0ef0d66994e70b7005544d0204c1a89611
gogotanaka <mail@tanakakazuki.com>
2016-01-29 01:26:26 +0900

    Implement main function

--- lib/chebyshev.rb
+++ lib/chebyshev.rb
@@ -1,4 +1,14 @@
 require "chebyshev/version"
+require 'yaml'
+require 'erubis'
 
 class Chebyshev
+  def self.expand(origin_file_path, yaml_file_path, result_file_path=nil)
+    erb = Erubis::Eruby.new(File.read origin_file_path)
+    erb.result(YAML.load(File.read yaml_file_path)).tap do |expand_file|
+      if result_file_path
+        File.write(result_file_path, expand_file)
+      end
+    end
+  end
 end
